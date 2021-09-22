cfd16f645d560aeaccb7ef69652d37bd58c94f16
Rex <binhua.tein@gmail.com>
2021-09-22 10:57:24 +0800

    Export HTML testing

--- projects/bopomofo/build-html.bat
+++ projects/bopomofo/build-html.bat
@@ -1,8 +1,8 @@
 @echo off
-set dist=./app/export-html-test
+set dist=./app/bopomofo-test
 rem set postfix=timestamp
 set project=./projects/bopomofo
-set main=main.js
+set main=test.js
 set htmlTemplate=index.tmpl
 set assets=assets
 set root=root
