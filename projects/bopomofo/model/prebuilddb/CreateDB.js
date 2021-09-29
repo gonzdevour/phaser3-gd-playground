1be97e031972861b67bbe90c2ddaf40c2fd6f9b4
Rex <binhua.tein@gmail.com>
2021-09-29 14:27:26 +0800

    Add 'freq' into indices

--- projects/bopomofo/model/prebuilddb/CreateDB.js
+++ projects/bopomofo/model/prebuilddb/CreateDB.js
@@ -1,6 +1,18 @@
 import loki from 'lokijs/src/lokijs.js';
 import { CharactersCollectionName, WordsCollectionName } from './Const.js';
 
+/*
+characters: [
+    {character, initials, media, vowel, tone, wid:[...]}.
+    ...
+]
+
+words: [
+    {word, freq, pid:[[...], [...]]},
+    ...
+]
+*/
+
 var CreateDB = function () {
     var db = new loki('bopomofo.db', {
         env: 'BROWSER'
@@ -8,10 +20,10 @@ var CreateDB = function () {
 
     db.addCollection(CharactersCollectionName, {
         indices: ['character', 'initials', 'media', 'vowel', 'tone']
-    })
+    });
 
     db.addCollection(WordsCollectionName, {
-        indices: ['word']
+        indices: ['word', 'freq']
     })
 
     return db;
