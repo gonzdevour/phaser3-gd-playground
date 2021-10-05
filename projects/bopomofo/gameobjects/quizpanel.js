4367cd31f425ede9161cc7d3ae0493637e9eb364
Rex <binhua.tein@gmail.com>
2021-10-05 14:34:58 +0800

    Add member structure of QuizPanel

--- projects/bopomofo/gameobjects/quizpanel.js
+++ projects/bopomofo/gameobjects/quizpanel.js
@@ -1,3 +1,21 @@
+/*
+
+- QuizPanel
+    - title
+    - Word
+        - Character
+            - _character : Label
+            - _bopomofo : Labels
+                - _initials : Label
+                - _media : Label
+                - _vowel : Label
+                - _tone : Label
+                - _tone0 : Label
+    - Choices
+    - actions : Labels
+
+*/
+
 import Character from "./character/Character.js";
 import Word from "./word/Word.js";
 import Choices from "./choices/Choices.js";
