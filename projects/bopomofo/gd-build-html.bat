24b3f68b45f821979d73e62a882b94cbd60c8bc3
gonzdevour <jyunru.li@gmail.com>
2021-10-07 10:09:58 +0800

    app build

---
+++ projects/bopomofo/gd-build-html.bat
@@ -0,0 +1,11 @@
+@echo off
+set dist=./app/bopomofo
+rem set postfix=timestamp
+set project=./projects/bopomofo
+set main=quiz.js
+set htmlTemplate=gd-index.tmpl
+set assets=assets
+set root=root
+cd ..
+cd ..
+npm run production
