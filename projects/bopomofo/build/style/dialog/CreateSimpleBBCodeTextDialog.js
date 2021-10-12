75303aa1e0c3c8cce66f873a68030eee2ca17511
Rex <binhua.tein@gmail.com>
2021-10-12 23:30:55 +0800

    Split method

--- projects/bopomofo/build/style/dialog/CreateSimpleBBCodeTextDialog.js
+++ projects/bopomofo/build/style/dialog/CreateSimpleBBCodeTextDialog.js
@@ -1,4 +1,5 @@
 import CreateRoundRectangleBackground from '../CreateRoundRectangleBackground.js';
+import CreateActionButtons from './CreateActionButtons.js';
 import { TransitionIn, TransitionOut } from '../TransitionMethods.js';
 
 const GetValue = Phaser.Utils.Objects.GetValue;
@@ -69,46 +70,6 @@ var CreateSimpleBBCodeTextDialog = function (parent, config) {
     }
 
     return dialog;
-}
-
-var CreateActionButtons = function (scene, hasOKButton, hasCancelButton) {
-    // Build UI
-    var oneButtonMode = hasOKButton && !hasCancelButton;
-    var buttons = scene.rexUI.add.sizer({
-        orientation: 'x'
-    })
-
-    if (oneButtonMode) {
-        buttons
-            .addSpace()
-            .add(
-                CreateLabel(scene, 'yes'),
-                { key: 'yes' }
-            )
-            .addSpace()
-    } else {
-        buttons
-            .add(
-                CreateLabel(scene, 'yes'),
-                { key: 'yes' }
-            )
-            .addSpace()
-            .add(
-                CreateLabel(scene, 'no'),
-                { key: 'no' }
-            )
-    }
-
-    return buttons;
-}
-
-var CreateLabel = function (scene, img) {
-    return scene.rexUI.add.label({
-        // background: CreateRoundRectangleBackground(scene, 20, undefined, 0xffffff, 2),
-        icon: scene.add.image(0, 0, img).setDisplaySize(90, 90),
-        // text: scene.rexUI.add.BBCodeText(0, 0, 'X', { fontFamily: 'DFKai-SB', fontSize: 60 }),
-        // space: { left: 20, right: 20, top: 20, bottom: 20, icon: 10 },
-    })
 }
 
 export default CreateSimpleBBCodeTextDialog;
