ed9d1676afd185f556f1e57f8f775cadedb53325
Rex <binhua.tein@gmail.com>
2021-10-13 10:52:29 +0800

    Put model at a global variable of scenes

--- projects/bopomofo/scenes/Base.js
+++ projects/bopomofo/scenes/Base.js
@@ -1,12 +1,24 @@
 import 'phaser';
 
 // Provide common methods or properties
+var Model;
+
 class Base extends Phaser.Scene {
     set model(value) {
-        this.registry.set('model', value);
+        // Model only can be assigned once
+        if (Model !== undefined) {
+            return;
+        }
+
+        Model = value;
+
+        // Free Model when game destroy
+        this.game.events.on('destroy', function () {
+            Model = undefined;
+        })
     }
     get model() {
-        return this.registry.get('model');
+        return Model;
     }
 }
 export default Base;
