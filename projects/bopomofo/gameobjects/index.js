2b3b5a68f7b18750dafc51263fd19321a0d575ca
Rex <binhua.tein@gmail.com>
2022-01-19 10:52:19 +0800

    Remove invalid import file

--- projects/bopomofo/gameobjects/index.js
+++ projects/bopomofo/gameobjects/index.js
@@ -3,13 +3,9 @@ import Word from "./word/Word.js";
 import Choices from "./choices/Choices.js";
 import QuizPanel from "./quizpanel/QuizPanel.js";
 
-import CharacterTable from "./charactertable/CharacterTable.js";
-
 export {
     Character,
     Word,
     Choices,
     QuizPanel,
-
-    CharacterTable,
 }
