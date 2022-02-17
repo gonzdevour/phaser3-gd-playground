e9bac2b007a5d11dd6aa3371af4542cea5a8b590
Rex <binhua.tein@gmail.com>
2022-02-17 10:14:07 +0800

    Rename extension of pack configuration file

--- plugins/packfolder/foldertojson/PackAssetFile.js
+++ plugins/packfolder/foldertojson/PackAssetFile.js
@@ -9,48 +9,45 @@ var PackAssetFile = function (type, child, config, totalPackResults) {
         packResult = { type: type, key: key };
     }
 
-    var url = GetURL(child.path, config.relatedPathFrom);
-
-    switch (type) {
-        case 'audio':
-            if (!packResult.url) {
-                packResult.url = [];
-            }
-            packResult.url.push(url);
-            break;
-
-        case 'atlas':
-            if (GetExtend(child.name) === '.json') {
-                packResult.atlasURL = url;
-            } else {
-                packResult.textureURL = url;
-            }
-            break;
-
-        case 'bitmapFont':
-            if (GetExtend(child.name) === '.xml') {
-                packResult.fontDataURL = url;
-            } else {
-                packResult.textureURL = url;
-            }
-            break;
-
-        case 'spritesheet':
-            if (GetExtend(child.name) === config.configYamlExtension) {
-                var packConfig = GetYamlObject(child.path);
-                for (var k in packConfig) {
-                    packResult[k] = packConfig[k];
+    var extName = GetExtend(child.name);
+    if (extName === config.configYamlExtension) {
+        // Is a pack config file
+        var packConfig = GetYamlObject(child.path);
+        for (var k in packConfig) {
+            packResult[k] = packConfig[k];
+        }
+    } else {
+        var url = GetURL(child.path, config.relatedPathFrom);
+        switch (type) {
+            case 'audio':
+                if (!packResult.url) {
+                    packResult.url = [];
                 }
-            } else {
-                packResult.url = url;
-            }
-            break;
+                packResult.url.push(url);
+                break;
 
-        default:
-            // image,text,json,animation,xml,svg,html,
-            // css,sceneFile,script,glsl,tilemapTiledJSON,tilemapCSV
-            packResult.url = url;
-            break;
+            case 'atlas':
+                if (extName === '.json') {
+                    packResult.atlasURL = url;
+                } else {
+                    packResult.textureURL = url;
+                }
+                break;
+
+            case 'bitmapFont':
+                if (extName === '.xml') {
+                    packResult.fontDataURL = url;
+                } else {
+                    packResult.textureURL = url;
+                }
+                break;
+
+            default:
+                // image,text,json,animation,xml,svg,html,spritesheet
+                // css,sceneFile,script,glsl,tilemapTiledJSON,tilemapCSV
+                packResult.url = url;
+                break;
+        }
     }
 
     return packResult;
