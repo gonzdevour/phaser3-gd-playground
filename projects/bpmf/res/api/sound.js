c78d743ccb96fc23ed68e0e71d7b4c3a583f209e
gonzdevour <jyunru.li@gmail.com>
2022-03-16 15:40:53 +0800

    app build

--- projects/bpmf/res/api/sound.js
+++ projects/bpmf/res/api/sound.js
@@ -41,7 +41,7 @@ class cdv_sound {
     var root = window.location.pathname;
     //var fileSrc = root.substring(0, root.lastIndexOf('/')+1) + this.urls.get(key, "mp3");
     //var fileSrc = root.substring(0, root.lastIndexOf('/')+1) + this.urls[key]['mp3'];
-    var fileSrc = cordova.file.applicationDirectory + this.urls[key]['mp3'];
+    var fileSrc = cordova.file.applicationDirectory + 'www/' + this.urls[key]['mp3'];
     log(fileSrc);
     return fileSrc;
   }
