121e257ff99c6fe984b45e1ab2c3936cee9d8857
Rex <binhua.tein@gmail.com>
2022-04-14 12:00:31 +0800

    List keys of config

--- plugins/exporter-preprocessor/Preprocessor.js
+++ plugins/exporter-preprocessor/Preprocessor.js
@@ -1,6 +1,7 @@
 var util = require("util");
 
 var Preprocessor = function (config) {
+    // config = {projectRoot, assetsFolder}
     console.log('Global preprocessor');
     console.log(util.inspect(config, { showHidden: false, depth: null }));
 }
