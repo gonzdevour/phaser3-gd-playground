da680d66b5ff61dbd677aa41dc4bf38203968229
Rex <binhua.tein@gmail.com>
2022-04-15 10:42:37 +0800

    A pseudocode for turning off events

--- projects/bopomofo/build/control/quiz/SetupQuizPanel.js
+++ projects/bopomofo/build/control/quiz/SetupQuizPanel.js
@@ -1,13 +1,17 @@
 var SetupQuizPanel = function (quizPanel, question, onSubmit) {
     // Fill quizPanel
     quizPanel
+        // 4. fire 'clear-events', to turn off events
+        .emit('clear-events')
         .clearChoices()
         .setTitle(question.title)
         .setWord(question.characters)
         .setChoicesText(question.createChoices())
         .layout()
 
-    // Note: make sure 'submit' is emitted (OK button clicked)    
+    // 1. Create a named callback
+    // var callback = function () { }
+    // Note: make sure 'submit' is emitted (OK button clicked)
     quizPanel
         .setData('focusCharacterIndex', question.characterIndex) // See CreateQuizPanel.js
         .once('submit', function (result) {
@@ -30,6 +34,13 @@ var SetupQuizPanel = function (quizPanel, question, onSubmit) {
             if (onSubmit) {
                 onSubmit(verifyResult);
             }
+        })
+
+        // 2. register event using this named callback
+        // .on('eventName', callback)        
+        .once('clear-events', function () {
+            // 3. remove event callback in 'clear-events' event
+            // quizPanel.off('eventName', callback);
         })
 
     return quizPanel;
