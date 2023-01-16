c46cfbe91268be51f61ebd826f74ef472d6f170b
Rex <binhua.tein@gmail.com>
2023-01-16 16:52:04 +0800

    Modify

--- plugins/utils/image/loadImageFromUrl.js
+++ plugins/utils/image/loadImageFromUrl.js
@@ -1,28 +1,33 @@
-var loadImageFromUrl = function(scene,key,url, LoadingProgress, progressUI, pseudoDuration){
+var loadImageFromUrl = function (scene, key, url, LoadingProgress, progressUI, pseudoDuration) {
     return new Promise(function (resolve, reject) {
 
-        var loadSuccess = function (key, type, data){
+        var loadSuccess = function (key, type, data) {
             console.log('load online image ' + key + ' complete');
-            resolve('loadImgSuccess');
+            onComplete();
+            resolve();
         }
-        
-        var loadError = function (fileObj){
+
+        var loadError = function (fileObj) {
             console.log('load online image ' + key + ' failed');
-            resolve('loadImageError');
+            onComplete();
+            reject();
+        }
+
+        var onComplete = function () {
+            scene.load
+                .off('filecomplete-image-' + key, loadSuccess)
+                .off('loaderror', loadError)
         }
 
         scene.load
-            .image(key,url)
-            .once('filecomplete-image-' + key, loadSuccess, scene)
-            .once('loaderror', loadError, scene)
+            .image(key, url)
+            .once('filecomplete-image-' + key, loadSuccess)
+            .once('loaderror', loadError)
             .start()
-        if (typeof(LoadingProgress === 'function')){
+
+        if (typeof (LoadingProgress === 'function')) {
             LoadingProgress(scene, progressUI, pseudoDuration);
         }
-    })
-    .then(function(){
-        scene.load.off('filecomplete-image-' + key, loadSuccess, scene)
-        scene.load.off('loaderror', loadError, scene)
     })
 }
 
