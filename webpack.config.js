ae0656cd7de837408426298c6b03e965d95d6ecb
Rex <binhua.tein@gmail.com>
2023-04-18 10:07:30 +0800

    Update phaser to 3.60.0

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,8 @@ plugins.push(
     new webpack.DefinePlugin({
         __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
         WEBGL_RENDERER: true, // I did this to make webpack work, but I'm not really sure it should always be true
-        CANVAS_RENDERER: true // I did this to make webpack work, but I'm not really sure it should always be true
+        CANVAS_RENDERER: true, // I did this to make webpack work, but I'm not really sure it should always be true        
+        "typeof WEBGL_DEBUG": JSON.stringify(false),
     })
 )
 
