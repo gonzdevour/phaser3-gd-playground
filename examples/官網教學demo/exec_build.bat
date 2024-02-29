f32107518f15f51e9ff3e50611da220a7a3c3220
gonzdevour <jyunru.li@gmail.com>
2024-02-29 15:59:45 +0800

    a

--- examples/官網教學demo/exec_build.bat
+++ examples/官網教學demo/exec_build.bat
@@ -3,12 +3,8 @@
 :: 獲取當前目錄的名稱
 for %%I in (.) do set CurrDirName=%%~nxI
 
-:: 獲取當前目錄的上一層目錄的名稱
-for %%I in (..) do set ParentDirName=%%~nxI
-
 set dist=./app/%CurrDirName%
-set project=./%ParentDirName%/%CurrDirName%
-
+set project=%CD%
 set main=main.js
 set htmlTemplate=settings/index.tmpl
 set assets=assets
